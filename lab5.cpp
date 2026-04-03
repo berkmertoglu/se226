962ab8c045cb301b03ec31e23abf4e9edea85dbe
berkmertoglu <mberk615@gmail.com>
2026-04-03 16:45:05 +0300

    Create lab5.cpp

---
+++ lab5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double geometricSum(int n, double r) {
+    // Base case
+    if (n == 0)
+        return 1;  // r^0 = 1
+
+    return pow(r, n) + geometricSum(n - 1, r);
+}
+
+int main() {
+    int n;
+    double r;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Enter common ratio r: ";
+    cin >> r;
+
+    double result = geometricSum(n, r);
+
+    cout << "Geometric Sum: " << result << endl;
+
+    return 0;
+}
